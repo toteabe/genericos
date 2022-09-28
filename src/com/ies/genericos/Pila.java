1613fc1263b4f2d86d6b6952d52a6085ef98b821
toteabe <toteabe@yahoo.es>
2022-09-28 22:31:14 +0200

    Commit de Pila

---
+++ src/com/ies/genericos/Pila.java
@@ -0,0 +1,89 @@
+package com.ies.genericos;
+
+import java.util.LinkedList;
+
+/**
+ * 
+ * 
+ * 
+ * toString(): devuelve en forma de String la información de la colección
+ * 
+ * @author user
+ *
+ * @param <T>
+ */
+
+public class Pila<T> {
+
+	private LinkedList<T> lista;
+
+	public Pila() {
+		this.lista = new LinkedList<>();
+	}
+
+	/**
+	 * Devuelve si la pila está vacía
+	 * 
+	 * @return true si pila vacía, falso si pila con algún valor
+	 */
+	public boolean estaVacia() {
+		return lista.isEmpty();
+	}
+
+	/**
+	 * Devuelve y elimina el primer elemento de la colección.
+	 * 
+	 * @return primer elemento
+	 */
+	public T extraer() {
+		return lista.poll();
+	}
+
+	/**
+	 * Devuelve el primer elemento de la colección
+	 * 
+	 * @return
+	 */
+	public T primero() {
+		return lista.getFirst();
+	}
+
+	/**
+	 * Añade un objeto por el extremo que corresponda.
+	 * 
+	 * @param t
+	 */
+	public void aniadir(T t) {
+		lista.push(t);
+	}
+
+	public String toString() {
+		StringBuffer strBuf = new StringBuffer("Respresentación de Pila");
+		
+		strBuf.append("<")
+			 	.append((lista.size() > 0 ? lista.getFirst().getClass().toString().replaceFirst("class ", ""): "pila vacía, no puedo decidir T"))
+				.append(">\n");
+		
+		strBuf.append("Resumen: pila con ").append( lista.size()).append(" elementos:\n");
+		for (T t : lista) {
+			strBuf.append(t).append("\n");
+		}
+		
+		return strBuf.toString();
+	}
+
+	public static void main(String[] args) {
+
+		Pila<String> pilaStr = new Pila<>();
+
+		pilaStr.aniadir("UNO");
+		pilaStr.aniadir("DOS");
+		pilaStr.aniadir("TRES");
+		System.out.println(pilaStr);
+
+		System.out.println((pilaStr.extraer()));
+		pilaStr.aniadir("CUATRO");
+		System.out.println(pilaStr);
+	}
+
+}
